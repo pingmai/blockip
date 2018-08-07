4dd8ed26f8e94cf74c1200ab5eef4a9bc8dad7c7
ping <ping@stepnet.com>
2018-08-06 21:44:04 -0700

    shell version of sshlog.py

---
+++ sshlog.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -f
+#
+# Check 'Invalid user' attempts on sshd in auth.log (LOGFILE).
+# If there are more than 5 (MAX_TRY) failed attempts within
+# the last hour (EARLIEST), source ip is added to PF_TABLE table in pf
+#
+MAX_TRY=3
+LOGFILE=/var/log/clients/ftp.pinelake.stepnet.com/auth.log
+# last 24 hour
+EARLIEST=`date -v-24H +%s`
+PF_TABLE=blocked3d
+
+grep "Invalid user" $LOGFILE | while read d0 d1 d2 rest; do
+	timestamp=`date -j -f "%b %d %H:%M:%S" "$d0 $d1 $d2" +%s`
+	test $timestamp -lt $EARLIEST && continue
+	echo $rest | cut -d ":" -f2- | cut -d " " -f6
+done | sort | uniq -c | while read count ip; do
+	test $count -lt $MAX_TRY && continue
+	pfctl -v -t $PF_TABLE -T add $ip
+done
