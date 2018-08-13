4211eefafd1b9241cc1e6e25530a3f306ea45512
ping <ping@stepnet.com>
2018-08-12 23:15:34 -0700

    check for existing ip in table

--- sshlog.sh
+++ sshlog.sh
@@ -10,11 +10,15 @@ LOGFILE=/var/log/clients/ftp.pinelake.stepnet.com/auth.log
 EARLIEST=`date -v-24H +%s`
 PF_TABLE=blocked3d
 
+tmpfile=`mktemp`
+trap 'rm -f $tmpfile' EXIT
+pfctl -t $PF_TABLE -T show > $tmpfile
+
 grep "Invalid user" $LOGFILE | while read d0 d1 d2 rest; do
 	timestamp=`date -j -f "%b %d %H:%M:%S" "$d0 $d1 $d2" +%s`
 	test $timestamp -lt $EARLIEST && continue
 	echo $rest | cut -d ":" -f2- | cut -d " " -f6
 done | sort | uniq -c | while read count ip; do
 	test $count -lt $MAX_TRY && continue
-	pfctl -v -t $PF_TABLE -T add $ip
+	grep -q $ip $tmpfile || pfctl -t $PF_TABLE -T add $ip
 done
